2a88a8804b19513967164c0922b2cd8fb6e180f4
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-11-05 03:38:27 +0000

    run-test.cpp: main(): print non-pointer variables

--- run-test.cpp
+++ run-test.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include "personType.h"
+#include <iomanip>
 
 using namespace std;
 
@@ -19,8 +20,19 @@ int main() {
     double price = 5.2;
     personType person("Quom", "Sandwich", "124 Firehouse Way", 76, "04-04-2026", 'M', 24);
 
-    // init pointers
 
+    cout << "Non-pointer variable values:" << endl;
+    // label (20) | value (12) | address (20)
+    cout << left << setw(20) << "x:"     << setw(12) << x      << setw(20) << "address:" << &x << endl;
+    cout << left << setw(20) << "y:"     << setw(12) << y      << setw(20) << "address:" << &y << endl;
+    cout << left << setw(20) << "z:"     << setw(12) << z      << setw(20) << "address:" << &z << endl;
+    cout << left << setw(20) << "c:"     << setw(12) << c      << setw(20) << "address:" << (void*)&c << endl;
+    cout << left << setw(20) << "price:" << setw(12) << price  << setw(20) << "address:" << &price << endl;
+    cout << "person: " << endl; person.print(); cout << endl;
+    cout << left << setw(20) << "address:" << &person << endl << endl;
+
+    // init pointers
+    
     int *xPtr = &x;
     int *yPtr = &y;
     int *zPtr = &z;
