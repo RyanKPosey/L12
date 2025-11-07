f463ea8fb8b51e28ed6fb48868db8c9a47062ae8
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-11-07 06:58:14 +0000

    run-test.cpp: main(): use getline() for string inputs

--- run-test.cpp
+++ run-test.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include "personType.h"
 #include <iomanip>
+#include <limits>
 
 using namespace std;
 
@@ -24,16 +25,20 @@ int main() {
 
     // get user input
     for (int i = 0; i < 5; i++) {
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cout << "Enter first name for person " << i + 1 << ": ";
-        cin >> firstName;
+        std::getline(cin, firstName);
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cout << "Enter last name for person " << i + 1 << ": ";
-        cin >> lastName;
+        std::getline(cin, lastName);
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cout << "Enter address for person " << i + 1 << ": ";
-        cin >> address;
+        std::getline(cin, address);
         cout << "Enter height (in inches) for person " << i + 1 << ": ";
         cin >> height;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cout << "Enter DOB for person " << i + 1 << ": ";
-        cin >> dob;
+        std::getline(cin, dob);
         cout << "Enter gender for person " << i + 1 << "(ex: 'M' or 'F'): ";
         cin >> gender;
         cout << "Enter age for person " << i + 1 << ": ";
