281daa4b6acb1539bc1997db8ac929e3fa991299
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-11-11 22:10:16 +0000

    run-test.cpp: main(): add intentional memory leaks

--- run-test.cpp
+++ run-test.cpp
@@ -15,24 +15,24 @@
 using namespace std;
 
 int main() {
-    personType person("Alesandra", "Sandwich", "123 Cookie St", 76.5, "1990-01-04", 'M', 33);
-    studentType student("Alice", "Allison", 4.5, "Junior", "S456");
-    professorType professor("Dr. Bob", "Builder", "456 Construction Ave", 80.0, "1985-05-12", 'M', 40, "E456","Engineering", "PhD");
+    personType* person = new personType("Alesandra", "Sandwich", "123 Cookie St", 76.5, "1990-01-04", 'M', 33);
+    studentType* student = new studentType("Alice", "Allison", 4.5, "Junior", "S456");
+    professorType* professor = new professorType("Dr. Bob", "Builder", "456 Construction Ave", 80.0, "1985-05-12", 'M', 40, "E456","Engineering", "PhD");
 
-    student.setMother(&person);
-    student.setFather(&professor);
+    student->setMother(person);
+    student->setFather(professor);
 
-    personType copyOfStudent = student; // Shallow copy
+    personType* copyOfStudent = new personType(*student); // Shallow copy
 
-    copyOfStudent.setFirstName("CopiedAlice");
-    copyOfStudent.setLastName("CopiedAllison");
-    copyOfStudent.setAddress("456 New Address St");
-    copyOfStudent.setAge(21);
-    copyOfStudent.setHeight(65.0);
-    copyOfStudent.setDOB("2002-02-02");
-    copyOfStudent.setGender('F');
+    copyOfStudent->setFirstName("CopiedAlice");
+    copyOfStudent->setLastName("CopiedAllison");
+    copyOfStudent->setAddress("456 New Address St");
+    copyOfStudent->setAge(21);
+    copyOfStudent->setHeight(65.0);
+    copyOfStudent->setDOB("2002-02-02");
+    copyOfStudent->setGender('F');
 
-    personType* people[4] = { &person, &student, &professor, &copyOfStudent };
+    personType* people[4] = { person, student, professor, copyOfStudent };
 
     for (const auto& p : people) {
         p->print();
