c204233a5c0a401fc4cf943a2b368d0807bd7aa5
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-11-24 06:08:59 +0000

    run-test.cpp: add driver code for L12.E

--- run-test.cpp
+++ run-test.cpp
@@ -1,52 +1,44 @@
 /*
     Programmer: Ryan Posey
-    Assignment: L12D
-    Purpose: Practice dynamic memory allocation
-    Due date: 11/16/2025
+    Assignment: L12E
+    Purpose: Demonstrate abstract base class polymorphism (personType)
+    Date: 11/24/2025
 */
 
 #include <iostream>
+#include <vector>
 #include "personType.h"
-#include <iomanip>
-#include <limits>
 #include "studentType.h"
 #include "professorType.h"
 
 using namespace std;
 
+
 int main() {
-	 // Create objects dynamically
-    personType* person = new personType("Alesandra", "Sandwich", "123 Cookie St", 76.5, "1990-01-04", 'M', 33);
-    studentType* student = new studentType("Alice", "Allison", 4.5, "Junior", "S456");
-    professorType* professor = new professorType("Dr. Bob", "Builder", "456 Construction Ave", 80.0, "1985-05-12", 'M', 40, "E456","Engineering", "PhD");
+    // Base class pointer variables
+    personType* person1 = new studentType("Jonathan", "Blueberry", 3.8, "Senior", "S12345");
+    personType* person2 = new professorType("Janathan", "Sandwich", "123 University St", 76.0, "1970-01-01", 'F', 55, "E456", "Computer Science", "PhD");
+    studentType student("Chocolate Bar", "Ice Cream", 3.9, "Senior", "S100");
+    professorType professor("Cheese", "Fondu", "456 College Ave", 68.0, "1965-05-05", 'M', 83.0, "E789", "Culinary Arts", "PhD");
 
-    student->setMother(person);
-    student->setFather(professor);
+    // Call overriden virtual methods
+    cout << "\n--- Polymorphic Print via Base Pointers ---\n";
+    person1->print();
+    cout << "-----------------------------\n";
+    person2->print();
+    cout << "\n-----PersonType& param test----\n";
+    cout << "\n--- Student equals professor test (professorType as arg)---\n";
+    student.equals(*person2) ? 
+        cout << "Student equals person2\n" : 
+        cout << "Student does not equal person2\n";
+    cout << "-----------------------------\n";
+    cout << "\n--- Professor equals student test (studentType as arg) ---\n";
+    professor.equals(*person1) ? 
+        cout << "Professor equals person1\n" : 
+        cout << "Professor does not equal person1\n";
+    cout << "-----------------------------\n";
 
-    personType* copyOfStudent = new personType(*student); // Shallow copy
-
-    copyOfStudent->setFirstName("CopiedAlice");
-    copyOfStudent->setLastName("CopiedAllison");
-    copyOfStudent->setAddress("456 New Address St");
-    copyOfStudent->setAge(21);
-    copyOfStudent->setHeight(65.0);
-    copyOfStudent->setDOB("2002-02-02");
-    copyOfStudent->setGender('F');
-
-    personType* people[4] = { person, student, professor, copyOfStudent };
-
-    for (const auto& p : people) {
-        p->print();
-        cout << "----------------------------------------" << endl;
-    }
-
-    cout << "\nClean up memory\n";
-    // iterate by reference so we can null out the array entries after delete
-    for (auto& p : people) {
-        delete p;
-        p = nullptr;
-    }
-
-    cout << "Memory has been freed\n";
-    return 0;
+    // Free heap memory
+    delete person1;
+    delete person2;
 }
